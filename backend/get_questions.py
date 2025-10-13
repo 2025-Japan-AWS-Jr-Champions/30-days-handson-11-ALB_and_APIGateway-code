5308e04c6323d79a69081568e2495d88a758d13f
chiaoi <chiaoi@pm.me>
2025-10-14 00:00:17 +0900

    update

--- backend/get_questions.py
+++ backend/get_questions.py
@@ -28,10 +28,6 @@ def lambda_handler(event, context):
 
     return {
         "statusCode": 200,
-        "headers": {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*",
-        },
         "body": json.dumps(
             {
                 "message": {
--- backend/get_ranking.py
+++ backend/get_ranking.py
@@ -52,9 +52,5 @@ def lambda_handler(event, context):
 
     return {
         "statusCode": 200,
-        "headers": {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*",
-        },
         "body": json.dumps(top_items),
     }
